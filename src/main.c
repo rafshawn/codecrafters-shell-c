bc2d2d2f751090adff85daebee29d0990b188eda
Shawn <rafshawn@yandex.com>
2026-03-10 23:26:31 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -7,6 +8,14 @@ int main(int argc, char *argv[]) {
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  // Wait for user command
+  char command[256];
+  fgets(command, sizeof(command), stdin);
+
+  // Command not found
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found\n", command);
 
   return 0;
 }
